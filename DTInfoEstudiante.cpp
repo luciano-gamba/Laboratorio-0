90816b8d82a2480a22aa1e87dfe99fdb805c7d55
luciano <luciano@luciano-SF20GM7-2023>
2025-04-07 22:43:38 -0300

    Merge origin/master
    Conflicts:
    	Estudiante.cpp
    	Estudiante.h
    	Informacion.cpp
    	Informacion.h
    	main.cpp
    	nbproject/Makefile-Debug.mk
    	nbproject/Makefile-impl.mk
    	nbproject/Makefile-variables.mk
    	nbproject/Package-Debug.bash
    	nbproject/configurations.xml
    	nbproject/private/private.xml

---
+++ DTInfoEstudiante.cpp
@@ -0,0 +1,29 @@
+#include "DTInfoEstudiante.h"
+
+DTInfoEstudiante::DTInfoEstudiante() {
+    ci=0;
+    NombreEst="Sin nombre";
+    identificadorInfo=0;
+}
+DTInfoEstudiante::DTInfoEstudiante(int a,std::string b,int c){
+    ci=a;
+    NombreEst=b;
+    identificadorInfo=c;
+}
+DTInfoEstudiante::~DTInfoEstudiante(){
+    
+}
+void DTInfoEstudiante::setInfo(int a,std::string b,int c){
+    ci=a;
+    NombreEst=b;
+    identificadorInfo=c;
+}
+int DTInfoEstudiante::getCi(){
+    return ci;
+}
+std::string DTInfoEstudiante::getNombreEst(){
+    return NombreEst;
+}
+int DTInfoEstudiante::getidentificadorInfo(){
+    return identificadorInfo;
+}
