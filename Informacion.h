b9928ac24d10cc3da61d9a5317fb9fae69fb4346
luciano <luciano@luciano-SF20GM7-2023>
2025-04-07 22:44:32 -0300

    Merge origin/master
    Conflicts:
    	Estudiante.cpp
    	Estudiante.h
    	Informacion.cpp
    	Informacion.h
    	main.cpp
    	nbproject/Makefile-Debug.mk
    	nbproject/Makefile-impl.mk
    	nbproject/Makefile-variables.mk
    	nbproject/Package-Debug.bash
    	nbproject/configurations.xml
    	nbproject/private/private.xml

--- Informacion.h
+++ Informacion.h
@@ -1,22 +1,16 @@
 #ifndef INFORMACION_H
 #define INFORMACION_H
 #include <string>
-using namespace std;
-
-struct DTFecha_{
-    int dia;
-    int mes;
-    int anio;
-}DTFecha;
+#include "DTFecha.h"
 
 class Informacion {
-public:
-    Informacion(int id, int dia, int mes, int anio);
-    virtual ~Informacion();
-    virtual std::string toString();
-protected:
-    int identificador;
-    DTFecha_ Fecha;
+    public:
+        Informacion(int id, int dia, int mes, int anio);
+        virtual ~Informacion();
+        virtual std::string toString();
+    protected:
+        int identificador;
+        DTFecha Fecha;
 };
 
 #endif /* INFORMACION_H */
