c8b21d2eab0c690d1ebe9414a85b51cec7331c5c
luciano <luciano@luciano-SF20GM7-2023>
2025-04-07 22:43:53 -0300

    Merge origin/master
    Conflicts:
    	Estudiante.cpp
    	Estudiante.h
    	Informacion.cpp
    	Informacion.h
    	main.cpp
    	nbproject/Makefile-Debug.mk
    	nbproject/Makefile-impl.mk
    	nbproject/Makefile-variables.mk
    	nbproject/Package-Debug.bash
    	nbproject/configurations.xml
    	nbproject/private/private.xml

--- Informacion.cpp
+++ Informacion.cpp
@@ -1,16 +1,13 @@
 #include "Informacion.h"
-#include <iostream>
 
 Informacion::Informacion(int id, int dia, int mes, int anio){
     this->identificador = id;
-    this->Fecha.dia = dia;
-    this->Fecha.mes = mes;
-    this->Fecha.anio = anio;
+    this->Fecha.setfecha(dia,mes,anio);
 }
 
 Informacion::~Informacion(){
 }
 
-string Informacion::toString(){
-      return("Esta es la informacion: "+this->identificador+'\n'+std::to_string(this->Fecha.dia)+"/"+std::to_string(this->Fecha.mes)+"/"+std::to_string(this->Fecha.anio)+"/");
+std::string Informacion::toString(){
+    return("Esta es la informacion: "+this->identificador+'\n'+std::to_string(this->Fecha.getDia())+"/"+std::to_string(this->Fecha.getMes())+"/"+std::to_string(this->Fecha.getAnio())+"/");
 }
