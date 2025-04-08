95f790642f2c527e5eaf2351f094a2d54fe9cf40
luciano <luciano@luciano-SF20GM7-2023>
2025-04-07 22:41:52 -0300

    Merge origin/master
    Conflicts:
    	Estudiante.cpp
    	Estudiante.h
    	Informacion.cpp
    	Informacion.h
    	main.cpp
    	nbproject/Makefile-Debug.mk
    	nbproject/Makefile-impl.mk
    	nbproject/Makefile-variables.mk
    	nbproject/Package-Debug.bash
    	nbproject/configurations.xml
    	nbproject/private/private.xml

---
+++ DTFecha.cpp
@@ -0,0 +1,33 @@
+#include "DTFecha.h"
+
+DTFecha::DTFecha() {
+    dia=0;
+    mes=0;
+    anio=0;
+}
+
+void DTFecha::setfecha(int d, int m, int a){
+    dia=d;
+    mes=m;
+    anio=a;
+}
+int DTFecha::getDia(){
+    return dia;
+}
+int DTFecha::getMes(){
+    return mes;
+}
+int DTFecha::getAnio(){
+    return anio;
+}
+bool DTFecha::mayorIgual(DTFecha otra){
+    if(anio>otra.getAnio()){
+        return true;
+    }else if (anio==otra.getAnio() && mes>otra.getMes()){
+        return true;
+    }else if (anio==otra.getAnio() && mes==otra.getMes() && dia>=otra.getDia()){
+        return true;
+    }else{
+        return false;
+    }
+}
